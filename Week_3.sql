5102e2aa048ff65a525263d8393fb97356ef2d32
Oğuzhan Evcimen <oguzhanevcimen@hotmail.com>
2022-10-26 03:02:42 +0300

    Güncellendi

--- Week_3.sql
+++ Week_3.sql
@@ -1,4 +1,11 @@
---3.Hafta Sql Çalışmaları
---Bu hafta yaptığımız komutları buraya yazacağım
---Week3.
---Create View Oluşturma
+--Random maç skoru üretmek.T-Sql çalışması.
+DECLARE @EV_SAHIBI_TAKIM VARCHAR(50);
+DECLARE @EV_SAHIBI_TAKIM_SKOR tinyint;
+DECLARE @KONUK_TAKIM VARCHAR(50);
+DECLARE @KONUK_TAKIM_SKOR tinyint;
+SET @EV_SAHIBI_TAKIM = 'bjk'
+SET @EV_SAHIBI_TAKIM_SKOR = cast(rand()*7 as tinyint)
+SET @KONUK_TAKIM = 'ts'
+SET @KONUK_TAKIM_SKOR = cast(rand()*7 as tinyint)
+PRINT ' Tahminimce BJK-TS maçı ' + UPPER(@EV_SAHIBI_TAKIM) + ':' + CAST(@EV_SAHIBI_TAKIM_SKOR AS CHAR(1)) + ' ' +
+  + UPPER(@KONUK_TAKIM) + ':' + CAST(@KONUK_TAKIM_SKOR AS CHAR(1)) + ' olarak sonuçlanacak.'
